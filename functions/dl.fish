b5020bfa9d1e5c00b7f20a64f81762d87eb6853e
sam <hgl9@foxmail.com>
2026-03-06 17:47:45 +0800

    fix: dl for aria2c not work in fish.

--- functions/dl.fish
+++ functions/dl.fish
@@ -1,6 +1,6 @@
 function dl
     set -l np (nproc)
-    set -l lnp (math "np * 2")
-    set -l wnp (math "np * 3")
+    set -l lnp (math "$np * 2")
+    set -l wnp (math "$np * 3")
     aria2c -c -V -k1M -s$lnp -x16 -j$wnp --max-tries=3 --retry-wait=5 $argv
 end
